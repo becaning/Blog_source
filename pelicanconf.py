11bd2170cf30f7cb9df15ad96362ef6dbf6694f0
yongqiang.ren <973313003@qq.com>
2015-07-04 16:54:17 +0800

    修改pelicanconf.py配置文件

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,48 +2,48 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-#==========SITE===============
+# ==========SITE===============
 AUTHOR = u'becaning'
 SITENAME = u'花满楼'
 SITEURL = 'http://becaning.com'
 
 PATH = 'content'
 
-#==========THEME===============
+# ==========THEME===============
 THEME = 'Mtheme'
 
-#THEMECOLOR = '[united | slate | cosmo | cyborg | darkly | flatly | journal | sandstone]'
+# THEMECOLOR = '[united | slate | cosmo | cyborg | darkly | flatly | journal | sandstone]'
 THEMECOLOR = 'flatly'
 
-#==========TIMEZONE===============
+# ==========TIMEZONE===============
 TIMEZONE = 'Asia/Shanghai'
 LOCALE = 'zh_CN.UTF-8'
 
 DEFAULT_LANG = u'en'
 
-#=======CONTENT==========
+# =======CONTENT==========
 IGNORE_FILES = ['*.i.*','*.swp']
 
 
-#==========多说评论系统===============
+# ==========多说评论系统===============
 SHORT_NAME = 'becaning'
 
-# Feed generation is usually not desired when developing
-#FEED_ALL_ATOM = None
-#CATEGORY_FEED_ATOM = None
-#TRANSLATION_FEED_ATOM = None
+#  Feed generation is usually not desired when developing
+# FEED_ALL_ATOM = None
+# CATEGORY_FEED_ATOM = None
+# TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-#LINKS = (('Pelican', 'http://getpelican.com/'),
-#         ('Python.org', 'http://python.org/'),
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
 #         ('Jinja2', 'http://jinja.pocoo.org/'),
 #         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-#SOCIAL = (('You can add links in your config file', '#'),
+# SOCIAL = (('You can add links in your config file', '#'),
 #          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 100
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
